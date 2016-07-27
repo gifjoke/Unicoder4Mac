25db7644c1646d3f79d14d2d3482dda2314f3dec
Fnoz <fnoz@sina.com>
2016-07-27 18:24:42 +0800

    Unicoder初始化；

--- Unicoder/ViewController.swift
+++ Unicoder/ViewController.swift
@@ -9,11 +9,46 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    override func viewDidAppear() {
+        let win = view.window!
+        win.backgroundColor = NSColor.clearColor()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.init(red: 252/255.0, green: 252/255.0, blue: 250/255.0, alpha: 1).CGColor
+        view.layer?.cornerRadius = 5
+        
+        let topStartLabel = NSTextField.init(frame: CGRect.init(x: 8, y: view.frame.height - 55, width: 20, height: 20))
+        topStartLabel.stringValue = ">"
+        topStartLabel.bezeled = false
+        topStartLabel.drawsBackground = false
+        topStartLabel.editable = false
+        topStartLabel.selectable = false
+        topStartLabel.font = NSFont.systemFontOfSize(20)
+        topStartLabel.textColor = NSColor.init(red: 230/255.0, green: 75/255.0, blue: 21/255.0, alpha: 1)
+        view.addSubview(topStartLabel)
+        
+        let topTextView = NSTextView.init(frame: CGRect.init(x: 30, y: view.frame.height / 2, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
+        topTextView.backgroundColor = NSColor.yellowColor()
+        view.addSubview(topTextView)
+        
+        let bottomStartLabel = NSTextField.init(frame: CGRect.init(x: 8, y: view.frame.height / 2 - 45, width: 20, height: 20))
+        bottomStartLabel.stringValue = ">"
+        bottomStartLabel.bezeled = false
+        bottomStartLabel.drawsBackground = false
+        bottomStartLabel.editable = false
+        bottomStartLabel.selectable = false
+        bottomStartLabel.font = NSFont.systemFontOfSize(20)
+        bottomStartLabel.textColor = NSColor.init(red: 230/255.0, green: 75/255.0, blue: 21/255.0, alpha: 1)
+        view.addSubview(bottomStartLabel)
+        
+        let bottomTextView = NSTextView.init(frame: CGRect.init(x: 30, y: 10, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
+        bottomTextView.backgroundColor = NSColor.yellowColor()
+        view.addSubview(bottomTextView)
     }
 
     override var representedObject: AnyObject? {
