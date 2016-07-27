b58250ed463c4e953120cd81c49be40f4779da3b
LuWeijun <luweijun@weidian.com>
2016-07-27 16:53:49 +0800

    Initial Commit

---
+++ Unicoder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Unicoder
+//
+//  Created by Fnoz on 16/7/27.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Unicoder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Unicoder
+//
+//  Created by Fnoz on 16/7/27.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
