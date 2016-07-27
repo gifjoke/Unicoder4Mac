ddea08c69da688c46e183d99b1658e78a839a5d6
Fnoz <fnoz@sina.com>
2016-07-27 23:54:02 +0800

    Add app icon.

--- Unicoder/ViewController.swift
+++ Unicoder/ViewController.swift
@@ -8,11 +8,15 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextViewDelegate {
     
+    var topTextView:NSTextView! = nil
+    var bottomTextView:NSTextView! = nil
     override func viewDidAppear() {
         let win = view.window!
         win.backgroundColor = NSColor.clearColor()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(update(_:)), name: NSTextViewDidChangeSelectionNotification, object: nil)
     }
 
     override func viewDidLoad() {
@@ -22,7 +26,7 @@ class ViewController: NSViewController {
         view.layer?.backgroundColor = NSColor.init(red: 252/255.0, green: 252/255.0, blue: 250/255.0, alpha: 1).CGColor
         view.layer?.cornerRadius = 5
         
-        let topStartLabel = NSTextField.init(frame: CGRect.init(x: 8, y: view.frame.height - 55, width: 20, height: 20))
+        let topStartLabel = NSTextField.init(frame: CGRect.init(x: 8, y: view.frame.height - 52, width: 20, height: 20))
         topStartLabel.stringValue = ">"
         topStartLabel.bezeled = false
         topStartLabel.drawsBackground = false
@@ -32,11 +36,15 @@ class ViewController: NSViewController {
         topStartLabel.textColor = NSColor.init(red: 230/255.0, green: 75/255.0, blue: 21/255.0, alpha: 1)
         view.addSubview(topStartLabel)
         
-        let topTextView = NSTextView.init(frame: CGRect.init(x: 30, y: view.frame.height / 2, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
-        topTextView.backgroundColor = NSColor.yellowColor()
+        topTextView = NSTextView.init(frame: CGRect.init(x: 30, y: view.frame.height / 2, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
+        topTextView.backgroundColor = NSColor.clearColor()
+        topTextView.insertionPointColor = NSColor.init(red: 108/255.0, green: 113/255.0, blue: 196/255.0, alpha: 1)
+        topTextView.textColor = NSColor.init(red: 108/255.0, green: 113/255.0, blue: 196/255.0, alpha: 1)
+        topTextView.font = NSFont.systemFontOfSize(20)
+        topTextView.delegate = self
         view.addSubview(topTextView)
         
-        let bottomStartLabel = NSTextField.init(frame: CGRect.init(x: 8, y: view.frame.height / 2 - 45, width: 20, height: 20))
+        let bottomStartLabel = NSTextField.init(frame: CGRect.init(x: 8, y: view.frame.height / 2 - 42, width: 20, height: 20))
         bottomStartLabel.stringValue = ">"
         bottomStartLabel.bezeled = false
         bottomStartLabel.drawsBackground = false
@@ -46,9 +54,32 @@ class ViewController: NSViewController {
         bottomStartLabel.textColor = NSColor.init(red: 230/255.0, green: 75/255.0, blue: 21/255.0, alpha: 1)
         view.addSubview(bottomStartLabel)
         
-        let bottomTextView = NSTextView.init(frame: CGRect.init(x: 30, y: 10, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
-        bottomTextView.backgroundColor = NSColor.yellowColor()
+        bottomTextView = NSTextView.init(frame: CGRect.init(x: 30, y: 10, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
+        bottomTextView.editable = false
+        bottomTextView.backgroundColor = NSColor.clearColor()
+        bottomTextView.font = NSFont.systemFontOfSize(20)
+        bottomTextView.textColor = NSColor.init(red: 108/255.0, green: 113/255.0, blue: 196/255.0, alpha: 1)
         view.addSubview(bottomTextView)
+    }
+    
+    func update(noti:NSNotification) {
+        let textView = noti.object as! NSTextView
+        if textView == topTextView {
+            let string = textView.string
+            bottomTextView.string = unicodeToString(string!) as String
+        }
+    }
+    
+    func unicodeToString(string:NSString) -> NSString {
+        let tempStr1 = string.stringByReplacingOccurrencesOfString("\\u", withString: "\\U")
+        let tempStr2 = tempStr1.stringByReplacingOccurrencesOfString("\"", withString: "\"")
+        let tempStr3 = "\"".stringByAppendingString(tempStr2).stringByAppendingString("\"")
+        let tempData = tempStr3.dataUsingEncoding(NSUTF8StringEncoding)
+        var resultStr = ""
+        if let result = try? NSPropertyListSerialization.propertyListWithData(tempData!, options: .Immutable, format: nil) {
+            resultStr = result as! String
+            return result as! NSString}
+        return resultStr
     }
 
     override var representedObject: AnyObject? {
@@ -56,7 +87,6 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
 
 }
 
