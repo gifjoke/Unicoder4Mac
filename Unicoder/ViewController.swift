602e2a53ee7e8031357d972f5f12f3d8b2f75005
Fnoz <fnoz@sina.com>
2016-08-11 18:25:15 +0800

    好了，可以用checkbox工作了；

--- Unicoder/ViewController.swift
+++ Unicoder/ViewController.swift
@@ -50,7 +50,7 @@ class ViewController: NSViewController, NSTextViewDelegate {
         topStartLabel.textColor = NSColor.init(red: 230/255.0, green: 75/255.0, blue: 21/255.0, alpha: 1)
         view.addSubview(topStartLabel)
         
-        topTextView = NSTextView.init(frame: CGRect.init(x: 30, y: view.frame.height / 2 - 2, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
+        topTextView = NSTextView.init(frame: CGRect.init(x: 30, y: view.frame.height / 2 - 2, width: view.frame.width - 100 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
         topTextView.backgroundColor = NSColor.clearColor()
         topTextView.insertionPointColor = NSColor.init(red: 108/255.0, green: 113/255.0, blue: 196/255.0, alpha: 1)
         topTextView.textColor = NSColor.init(red: 108/255.0, green: 113/255.0, blue: 196/255.0, alpha: 1)
@@ -68,7 +68,7 @@ class ViewController: NSViewController, NSTextViewDelegate {
         bottomStartLabel.textColor = NSColor.init(red: 230/255.0, green: 75/255.0, blue: 21/255.0, alpha: 1)
         view.addSubview(bottomStartLabel)
         
-        bottomTextView = NSTextView.init(frame: CGRect.init(x: 30, y: 10 - 1, width: view.frame.width - 10 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
+        bottomTextView = NSTextView.init(frame: CGRect.init(x: 30, y: 10 - 1, width: view.frame.width - 100 - 30, height: (view.frame.height - 10 * 2 - 50) / 2))
         bottomTextView.editable = false
         bottomTextView.backgroundColor = NSColor.clearColor()
         bottomTextView.font = NSFont.init(name: "Avenir Next", size: 18)
@@ -76,27 +76,29 @@ class ViewController: NSViewController, NSTextViewDelegate {
         view.addSubview(bottomTextView)
         
         //EncodeType
-        let encodeLabel = NSTextField.init(frame: CGRect.init(x: view.frame.width - 100, y: view.frame.height - 90, width: 75, height: 26))
-        encodeLabel.stringValue = "     Type"
-        encodeLabel.bezeled = false
-        encodeLabel.drawsBackground = false
-        encodeLabel.editable = false
-        encodeLabel.selectable = false
-        encodeLabel.font = NSFont.init(name: "Avenir Next", size: 15)
-        encodeLabel.wantsLayer = true
-        encodeLabel.layer?.borderColor = NSColor.lightGrayColor().CGColor
-        encodeLabel.layer?.borderWidth = 2
-        encodeLabel.textColor = NSColor.lightGrayColor()
-        view.addSubview(encodeLabel)
+        let encodeTypeLabel = NSTextField.init(frame: CGRect.init(x: view.frame.width - 85, y: view.frame.height - 90, width: 75, height: 26))
+        encodeTypeLabel.stringValue = "     Type"
+        encodeTypeLabel.bezeled = false
+        encodeTypeLabel.drawsBackground = false
+        encodeTypeLabel.editable = false
+        encodeTypeLabel.selectable = false
+        encodeTypeLabel.font = NSFont.init(name: "Avenir Next", size: 15)
+        encodeTypeLabel.wantsLayer = true
+        encodeTypeLabel.layer?.borderColor = NSColor.lightGrayColor().CGColor
+        encodeTypeLabel.layer?.borderWidth = 2
+        encodeTypeLabel.textColor = NSColor.lightGrayColor()
+        view.addSubview(encodeTypeLabel)
         
         let prototype = NSButtonCell.init()
         prototype.title = "EncodeType"
         prototype.setButtonType(.RadioButton)
-        typeMatrix = NSMatrix.init(frame: CGRect.init(x: view.frame.width - 100, y: view.frame.height - 200, width: 100, height: 100),
+        typeMatrix = NSMatrix.init(frame: CGRect.init(x: view.frame.width - 85, y: view.frame.height - 200, width: 100, height: 100),
                                    mode: .RadioModeMatrix,
                                    prototype: prototype,
                                    numberOfRows: 3,
                                    numberOfColumns: 1)
+        typeMatrix.target = self
+        typeMatrix.action = #selector(encodeTypeChanged(_:))
         var cellArray = typeMatrix.cells
         cellArray[0].title = "Unicode"
         cellArray[1].title = "URL"
@@ -104,30 +106,46 @@ class ViewController: NSViewController, NSTextViewDelegate {
         view.addSubview(typeMatrix)
         
         //EncodeDirection
-        let decodeLabel = NSTextField.init(frame: CGRect.init(x: view.frame.width - 100, y: view.frame.height - 290, width: 72, height: 26))
-        decodeLabel.stringValue = " Direction"
-        decodeLabel.bezeled = false
-        decodeLabel.drawsBackground = false
-        decodeLabel.editable = false
-        decodeLabel.selectable = false
-        decodeLabel.font = NSFont.init(name: "Avenir Next", size: 15)
-        decodeLabel.wantsLayer = true
-        decodeLabel.layer?.borderColor = NSColor.lightGrayColor().CGColor
-        decodeLabel.layer?.borderWidth = 2
-        decodeLabel.textColor = NSColor.lightGrayColor()
-        view.addSubview(decodeLabel)
+        let encodeDireLabel = NSTextField.init(frame: CGRect.init(x: view.frame.width - 85, y: view.frame.height - 290, width: 72, height: 26))
+        encodeDireLabel.stringValue = "     Dire"
+        encodeDireLabel.bezeled = false
+        encodeDireLabel.drawsBackground = false
+        encodeDireLabel.editable = false
+        encodeDireLabel.selectable = false
+        encodeDireLabel.font = NSFont.init(name: "Avenir Next", size: 15)
+        encodeDireLabel.wantsLayer = true
+        encodeDireLabel.layer?.borderColor = NSColor.lightGrayColor().CGColor
+        encodeDireLabel.layer?.borderWidth = 2
+        encodeDireLabel.textColor = NSColor.lightGrayColor()
+        view.addSubview(encodeDireLabel)
         
-        prototype.title = "EncodeDirection"
+        prototype.title = "EncodeDire"
         prototype.setButtonType(.RadioButton)
-        directionMatrix = NSMatrix.init(frame: CGRect.init(x: view.frame.width - 100, y: view.frame.height - 400, width: 100, height: 100),
+        directionMatrix = NSMatrix.init(frame: CGRect.init(x: view.frame.width - 85, y: view.frame.height - 400, width: 100, height: 100),
                                         mode: .RadioModeMatrix,
                                         prototype: prototype,
                                         numberOfRows: 2,
                                         numberOfColumns: 1)
+        directionMatrix.target = self
+        directionMatrix.action = #selector(encodeDireChanged(_:))
         cellArray = directionMatrix.cells
         cellArray[0].title = "Encode"
         cellArray[1].title = "Decode"
         view.addSubview(directionMatrix)
+    }
+    
+    func encodeTypeChanged(typeMatrix:NSMatrix) {
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.2 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.update(NSNotification.init(name: "", object: ""))
+        }
+    }
+    
+    func encodeDireChanged(directionMatrix:NSMatrix) {
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.2 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.update(NSNotification.init(name: "", object: ""))
+        }
     }
     
     func update(noti:NSNotification) {
@@ -174,5 +192,6 @@ class ViewController: NSViewController, NSTextViewDelegate {
             return Base64Encode(string)
         }
     }
+    
 }
 
